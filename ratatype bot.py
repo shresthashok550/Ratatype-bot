f6efbe0b7b0b6d38c1a7c1339215440a74e13ded
Ashok Shrestha <67057717+shresthashok550@users.noreply.github.com>
2021-06-04 11:41:55 +0545

    Add files via upload

---
+++ ratatype bot.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import pyautogui
+import pyperclip
+import time
+browser = webdriver.Chrome("C:/Users/Dell/Downloads/Compressed/chromedriver")#change this path to your chromedriver path
+
+print('RataType Bot (ratatype.com)')
+username = input("Enter Email:")#enter your email
+password = input("Enter Password:") #enter your password
+browser.get('https://www.ratatype.com/login/')
+browser.find_element_by_id('email').send_keys(username)
+browser.find_element_by_id('password').send_keys(password,Keys.RETURN)
+time.sleep(10)
+browser.get("https://www.ratatype.com/typing-test/test/")
+while(True):
+    browser.find_element_by_xpath("//*[contains(text(),'start typing now')]").click()
+
+    start = input("Press s to start: ")
+    if(start == 's'):
+        copyPath = (browser.find_elements_by_class_name("mainTxt"))
+        for post in copyPath:
+            print(post.text)
+            copyTo = post.text    
+        pyperclip.copy(copyTo)
+        time.sleep(10)
+        twrite = pyperclip.paste()
+        inp_time = float(input("Enter time: "))
+        time.sleep(5)
+        pyautogui.FAILSAFE=False
+        pyautogui.typewrite(twrite,float(inp_time))
+        time.sleep(5)
+        try:
+            browser.find_element_by_xpath('//*[@id="complete"]/div[2]/div/div[2]/div/button').click()
+        except:
+            browser.find_element_by_xpath("//*[contains(text(), 'TAKE A TYPING TEST')]").click()
+        end = input("continue? (y or n)")
+        if(end == 'y'):
+            continue  
